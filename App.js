3e5e22140dd306afaeffe4429d7d6c45bd11c7a5
LaodeGhazy <ghazylaode002@gmail.com>
2024-09-03 17:01:30 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+import * as React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
 
-import MainScreen from './src/screens/MainScreen';
+import MainScreen from "./src/screens/MainScreen";
+import RecipeDetailScreen from "./src/screens/RecipeDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +13,21 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="MainScreen" component={MainScreen} options={{
-          headerShown: false,
-        }} />
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="RecipeDetailScreen"
+          component={RecipeDetailScreen}
+          options={{
+            title: "RecipeDetail",
+            headerTransparent: true,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
